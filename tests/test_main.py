2935598365a0a278a7091cf16bd10fc352f541bb
Anatoly <30874961+anatoly-kor@users.noreply.github.com>
2022-04-18 10:25:09 +0300

    fix fat-finger issue (#3)
    Исправлена ошибка в слове Автор в тестах

--- tests/test_main.py
+++ tests/test_main.py
@@ -31,7 +31,7 @@ def test_main_file():
 
 def test_whats_new(mock_session):
     got = main.whats_new(mock_session)
-    header = ('Ссылка на статью', 'Заголовок', 'Редактор, Aвтор')
+    header = ('Ссылка на статью', 'Заголовок', 'Редактор, Автор')
     assert isinstance(got, list), (
         'Функция `whats_new` должна возвращать объект типа `list`'
     )
